e343eb97c96f9e76a0e16380cbf126dd25206f06
Alfian Kafilah Baits <alfiank@stelzers.co>
2025-05-14 08:55:17 +0700

    fix

--- resources/views/registration.blade.php
+++ resources/views/registration.blade.php
@@ -136,6 +136,9 @@
             background: #3300cc;
         }
     </style>
+    @if (file_exists(public_path('build/manifest.json')) || file_exists(public_path('hot')))
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
+    @endif
 </head>
 
 <body>
