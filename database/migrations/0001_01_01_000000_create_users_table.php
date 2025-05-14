cd8866f3fb439beab1b5d69ece854d3ed759a7c1
Alfian Kafilah Baits <alfiank@stelzers.co>
2025-05-14 11:46:31 +0700

    use soft deletes

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -18,6 +18,7 @@ return new class extends Migration
             $table->string('name');
             $table->string('nik');
             $table->string('telephone');
+            $table->softDeletes();
             $table->timestamps();
         });
 
